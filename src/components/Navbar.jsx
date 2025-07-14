4beeedf3410b4f9529d7aa444f7647bf160e733f
Jasmin <107794384+ginishajasmin@users.noreply.github.com>
2025-07-14 12:06:56 +0530

    nav icons completed
    nav icons completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { HiMenu, HiX } from 'react-icons/hi';
 
 const Navbar = () => {
 
@@ -21,7 +22,7 @@ const Navbar = () => {
   ]
 
   return (
-    <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100'>
+    <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
       <div className='w-full container mx-auto flex item-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
         {/* logo */}
         <div className='flex items-center gap-1 cursor-pointer'>
@@ -29,14 +30,29 @@ const Navbar = () => {
           <div className='w-4 h-4 bg-red-600 -ml-2 rounded-full opacity-100 hover:opacity-75 transition-opacity'></div>
         </div>
 
+        {/* mobile menu button */}
+        <button>
+          {isMenuOpen ? <HiX/> : <HiMenu/>}
+        </button>
+
         {/* desktop nav items */}
         <div className='hidden md:flex items-center gap-10'>
           {
             navLinks.map((link, index) => (
-              <a key={index} href={link.href} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all`}>{link.label}</a>
+              <a
+                onClick={() => setActiveLink(link.href)}
+                key={index} href={link.href} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${activeLink === link.href ? 'text-blue-600 after:w-full' : 'text-gray-600 hover:text-gray-900'}`}>{link.label}</a>
             ))
           }
         </div>
+
+        {/* Get in touch button */}
+        <div className='hidden md:flex items-center gap-4'>
+          <button className='md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
+            <a href="#newsletter">Get in touch</a>
+          </button>
+        </div>
+
       </div>
     </nav>
   )
